c99bd0eaf7ed7b0db7b920c67184601c5a6ad8c7
Marwane Rezgui <60197927+marbawws@users.noreply.github.com>
2023-01-04 22:31:54 -0500

    Delete GmailConfig.js

--- Skeleton-2.0.4/scripts/widgets/mail/GmailConfig.js
+++
@@ -1,4 +0,0 @@
-//gmail
-var GCLIENT_ID = '841784259300-9vtjumuvf740408fpo7thbqened3mmrq.apps.googleusercontent.com';
-var GAPI_KEY = 'AIzaSyBVTkPEZTKa8Uo_U56hh1YfAsFRnMxALLU';
-var gmailAddressTEMPORARY = 'maladin.rezgui@gmail.com'
