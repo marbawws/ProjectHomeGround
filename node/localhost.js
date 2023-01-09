98ff07f7b99907d08a44abb0f0f211168c74c7b0
marbawws <clibeau2010@hotmail.com>
2023-01-08 23:41:04 -0500

    restructure

---
+++ node/localhost.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const express = require('express');
+const url = require('url');
+const request = require('request');
+const { JSDOM } = jsdom;
+
+const hostname = '127.0.0.1';
+const port = 8000;
+const app = express();
+const router = express.Router();
+app.use('/scripts', express.static(path.join(__dirname + 'scripts')));
+app.use('/images', express.static(path.join(__dirname +'images')));
+app.use('/css', express.static(path.join(__dirname +'css')));
+
+// Setup essential routes
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '\\..\\index.html'));
+
+});
+router.get('/get-page-title',function(req, res) {
+  const pageURL = req.query.url;
+  const userAgent = req.query.userAgent;
+  if(pageURL !== undefined && userAgent !== undefined){
+    try {
+      request.get({
+        url: pageURL,
+        headers: {
+          'User-Agent' : userAgent
+        }
+      },(error, response, body)=>{
+        // console.log(body);
+        if (!error) {
+          //console.log(response);
+          const dom = new JSDOM(body);
+          res.status(200).send(dom.window.document.querySelector('title').textContent);
+        } else{
+          console.log(error);
+        }
+      });
+    } catch (e){
+      console.log(e);
+    }
+  } else{
+    res.status(204);
+  }
+  console.log(pageURL + "   " + userAgent);
+});
+
+// 'D:\\bureau\\wallpaper\\ProjectHomeGround\\Skeleton-2.0.4\\index.html\\'
+//add the router
+app.use('/', router);
+app.listen(8000);
+console.log('Running at Port 8000');
+
