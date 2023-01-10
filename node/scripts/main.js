ddcd4174004dc4cfa28231586f643b4ae0e0ab4e
marbawws <clibeau2010@hotmail.com>
2023-01-09 23:54:20 -0500

    learning react further

--- node/scripts/main.js
+++ node/scripts/main.js
@@ -2,26 +2,91 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
+class TabHeader extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { liked: false };
     }
 
     render() {
-        if (this.state.liked) {
-            return 'You liked this.';
-        }
-
         return (
-            <button onClick={() => this.setState({ liked: true })}>
-                Like
-            </button>
+            <button id={this.props.id} onClick={this.props.onClickedCallback}> {this.props.name}</button>
         );
+    }
+}
+
+class Tab extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { active:false}
+    }
+
+    render() {
+        if(this.state.active){
+            return (
+                <div style={{display: "block"}} id={this.props.id}>{this.props.id}</div>
+            );
+        } else{
+            return (
+                <div style={{display: "none"}} id={this.props.id} >{this.props.id}</div>
+            );
+        }
+    }
+}
+
+class Tabs extends React.Component {
+    state = {
+        counter: 0,
+        tabNames: [],
+        tabActive:[]
+    };
+    handleClickedTabHeader =(id) =>{
+        this.setState(prevState => ({
+
+        }));
+    };
+    render() {
+        return (
+            <React.StrictMode>
+                <div>
+                    <div>
+                        {[...Array(this.state.counter)].map((_, index) => (
+                            <TabHeader
+                                id={"reactHeader" + index}
+                                index={index} key={index}
+                                name={this.state.tabNames[index]}
+                                onClickedCallback={this.handleClickedTabHeader(index)}
+                            />
+                        ))}
+                        <button onClick={() => {
+                            this.generateNewTab({name:"test"});
+                        }}>
+                            Add new tab
+                        </button>
+                    </div>
+                    <div>
+                        {[...Array(this.state.counter)].map((_, index) => (
+                            <Tab
+                                id={"reactTab" + index}
+                                key={index}
+                                name={this.state.tabNames[index]}
+                                active={this.state.tabActive[index]}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </React.StrictMode>
+        );
+    }
+
+    generateNewTab(props) {
+        this.setState(prevState => ({
+            counter: prevState.counter + 1,
+            tabNames: [...prevState.tabNames, props.name]
+        }));
     }
 }
 
 const domContainer = document.querySelector('#tabTest');
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+root.render(e(Tabs));
 // Display a "Like" <button>
